0302e8e0c340afa71622ee20fab941ed8e3e5534
zappia7 <giuliazappia@ucsb.edu>
2022-11-18 14:55:33 -0800

    Giulia

--- Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -3,22 +3,22 @@ using UnityEngine.PostProcessing;
 
 namespace UnityEditor.PostProcessing
 {
-    //[CustomPropertyDrawer(typeof(MinAttribute))]
+    [CustomPropertyDrawer(typeof(MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            //MinAttribute attribute = (MinAttribute)base.attribute;
+            MinAttribute attribute = (MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
                 int v = EditorGUI.IntField(position, label, property.intValue);
-                //property.intValue = (int)Mathf.Max(v, attribute.min);
+                property.intValue = (int)Mathf.Max(v, attribute.min);
             }
             else if (property.propertyType == SerializedPropertyType.Float)
             {
                 float v = EditorGUI.FloatField(position, label, property.floatValue);
-                //property.floatValue = Mathf.Max(v, attribute.min);
+                property.floatValue = Mathf.Max(v, attribute.min);
             }
             else
             {
